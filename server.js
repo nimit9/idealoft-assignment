eb7d96f09c94017832d1cfae3e563afbfc35e58c
nimitharia <nimitharia007@gmail.com>
2022-11-11 16:44:03 +0530

    resolved path

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use("/api/v1/admin", authenticateUser, checkAdmin, adminRouter);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
     app.get("*", function (req, res) {
-        res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
